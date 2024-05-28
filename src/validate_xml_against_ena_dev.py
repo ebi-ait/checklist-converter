c157fc74110bd1cf51b98439ffa4f90a923856c4
ESapenaVentura <sapenaventura@gmail.com>
2024-05-28 15:16:24 +0100

    ck-20 now: clean docs on run, create proper structure to store validation

--- src/validate_xml_against_ena_dev.py
+++ src/validate_xml_against_ena_dev.py
@@ -1,17 +1,23 @@
+import os
+
 import requests
 
 from argparse import ArgumentParser, Namespace
 from io import StringIO
-import os
+from uuid import uuid4
 from os.path import join, isfile, isdir
+from xml.etree import ElementTree
+from time import sleep
+import re
 import glob
+import tqdm
 from typing import List
 
 def str_or_file_path(value: str) -> List:
     if isfile(value):
         return [value]
     elif isdir(value):
-        return glob.glob(join(value, "*.xml"))
+        return glob.glob(join(value, "**/*.xml"))
     raise ValueError("Input provided is not a file or an existing directory. Please provide proper input.")
 
 
@@ -48,32 +54,56 @@ def create_submission_xml():
     return submission_xml
 
 
+def clean_document(xml_document: str) -> str:
+    """
+    Remove accession identifiers from ENA XML document. They are not allowed for ENA validation.
+    :param xml_document:
+    :return:
+    """
+    root = ElementTree.XML(xml_document)
+    for parent in root.iter():
+        for child in list(parent):
+            if child.tag == 'SAMPLE':
+                del child.attrib['accession']
+                child.attrib['alias'] = str(uuid4())
+            if child.tag == 'IDENTIFIERS':
+                parent.remove(child)
+    return ElementTree.tostring(root).decode()
+
+
 def submit(sub_xml_streamable, sample_xml_streamable, url, username, password):
     r = requests.post(url, files={'SUBMISSION': sub_xml_streamable, 'SAMPLE': sample_xml_streamable},
                       auth=(username, password))
+    sleep(0.02)  # hard-capping rate of requests to 50 per second (https://ena-docs.readthedocs.io/en/latest/retrieval/programmatic-access.html#rate-limits)
     return r
+
+
+def retrieve_receipt_result(xml_receipt: str):
+    root = ElementTree.XML(xml_receipt)
+    return 'valid' if root.attrib['success'] == 'true' else 'invalid'
+
 
 def main(username, password, input_paths, output_path):
     submission_xml = create_submission_xml()
     submission_xml_streamable = StringIO(submission_xml)
     submission_url = "https://wwwdev.ebi.ac.uk/ena/submit/drop-box/submit/"
 
-    responses = {}
-    for sample_path in input_paths:
+    for sample_path in tqdm.tqdm(input_paths):
         submission_xml_streamable.seek(0)
-        output_name_no_extension = sample_path.split('/')[-1][:-4]
-        with open(sample_path, 'r') as sample_streamable:
-            try:
-                response = submit(submission_xml_streamable, sample_streamable, submission_url, username, password)
-                os.makedirs(f"{join(output_path, str(response.status_code))}", exist_ok=True)
-                full_output_path = f"{join(output_path, str(response.status_code), output_name_no_extension)}.xml"
-                with open(full_output_path, 'w') as f:
-                    f.write(response.text)
-            except Exception as e:
-                print(e)
-                full_output_path = f"{join(output_path, 'error', output_name_no_extension)}.xml"
-                with open(full_output_path, 'w') as f:
-                    f.write(response.text)
+        output_name = sample_path.split('/')[-1]
+        with open(sample_path, 'r') as f:
+            sample = clean_document(f.read())
+        sample_streamable = StringIO(sample)
+        response = submit(submission_xml_streamable, sample_streamable, submission_url, username, password)
+        receipt = response.text
+        receipt_tag = retrieve_receipt_result(receipt)
+        checklist_id = re.search("ERC\\d{6}", sample_path).group()
+
+        sample_out_path = join(output_path, f"{checklist_id}/{receipt_tag}")
+        if not isdir(sample_out_path):
+            os.makedirs(sample_out_path)
+        with open(join(sample_out_path, output_name), 'w') as f:
+            f.write(response.text)
 
 
 if __name__ == '__main__':
