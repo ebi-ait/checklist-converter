e5c1ae047528bd86d0969df18f568619b26abf0b
vijayaraja <vijayaraja@ebi.ac.uk>
2024-06-20 14:10:06 +0100

    code cleanup

--- invalid_sample_generator.py
+++ invalid_sample_generator.py
@@ -2,6 +2,7 @@ import requests
 import csv
 import xml.etree.ElementTree as ET
 import json
+import random
 
 
 def parse_csv_to_dict(file_path):
@@ -23,12 +24,7 @@ def get_ena_checklist(checklist_id):
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-
-    if response.status_code == 200:
-        return response.content
-    else:
-        print(f'Failed to fetch data. HTTP Status Code: {response.status_code}')
-
+    return response.content
 
 def get_biosample(biosample_id):
     url = 'https://www.ebi.ac.uk/biosamples/samples/'+biosample_id
@@ -38,16 +34,12 @@ def get_biosample(biosample_id):
     }
 
     response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f'Failed to fetch data. HTTP Status Code: {response.status_code}')
-        return None
-
-def add_mandatory_element_error(checklist_xml_str, biosample_json):
+def add_mandatory_element_error(checklist_xml_str, biosample_json, biosample_id):
     root = ET.fromstring(checklist_xml_str)
-
+    #print("biosample-json:",biosample_json)
     #find mandatory fields
     mandatory_fields = []
     for field in root.findall('.//FIELD'):
@@ -55,30 +47,26 @@ def add_mandatory_element_error(checklist_xml_str, biosample_json):
         name = field.find('NAME')
 
         if mandatory is not None and name is not None and mandatory.text == 'mandatory':
-            print(f'Found tropical_only: {name.text}')
             mandatory_fields.append(name.text)
     print('Mandatory fields:',mandatory_fields)
 
     #now remove mandatory any of the mandatory field from json randomly and write to file
-    # Remove the specific element
-    #This has to by dynamic and random , now just for testing popping it
-    element_to_remove = mandatory_fields.pop()
-    if element_to_remove in biosample_json["characteristics"]:
-        del biosample_json["characteristics"][element_to_remove]
-    else:
-        raise ValueError("Element" + element_to_remove +" Not present in biosample")
+    if mandatory_fields :
+        element_to_remove = mandatory_fields[random.randint(0, len(mandatory_fields)-1)]
+        if element_to_remove in biosample_json["characteristics"]:
+            del biosample_json["characteristics"][element_to_remove]
+        else:
+            raise ValueError("Element" + element_to_remove +" Not present in biosample")
 
-    updated_json_str = json.dumps(biosample_json, indent=4)
-
-    # write it into file
-    print(updated_json_str)
+        with open('./data/invalid'+biosample_id+'_mandatory.json', 'w') as file:
+            json.dump(biosample_json, file, indent=4)
 
 
 if __name__ == '__main__':
     accession_checklist_id_dict = parse_csv_to_dict('./data/accessions.csv')
     for accession, checklist in accession_checklist_id_dict.items():
-        print(accession+' <-acc,checklist--> '+checklist)
+        print(accession+' <-acc,checklist-> '+checklist)
         ena_checklist_xml = get_ena_checklist(checklist)
         biosample_json = get_biosample(accession)
-        add_mandatory_element_error(ena_checklist_xml, accession)
+        add_mandatory_element_error(ena_checklist_xml, biosample_json, accession)
 
